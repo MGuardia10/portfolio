ad12abbe90903b572228a7d2c9a744ec7f98d47b
Miguel Guardia <mike.guardia0309@gmail.com>
2025-11-09 19:07:27 +0100

    updates

--- src/about/components/ShowItems.jsx
+++ src/about/components/ShowItems.jsx
@@ -1,10 +1,9 @@
 import clsx from "clsx";
 
 function Items({ area, company, logo, url, date, key }) {
-
   const content = (
     <>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 min-w-0">
         <img
           src={logo}
           alt={company}
@@ -16,11 +15,21 @@ function Items({ area, company, logo, url, date, key }) {
           )}
         />
         <div className="flex flex-col gap-px">
-          <p className={`text-title-light dark:text-title-dark ${url ? "external-arrow" : ""}`}>{area}</p>
+          <p
+            className={`text-title-light dark:text-title-dark ${
+              url ? "external-arrow" : ""
+            }`}
+          >
+            {area}
+          </p>
           <p className="text-text-light dark:text-text-dark">{company}</p>
         </div>
       </div>
-      {date && <time className="text-text-light dark:text-text-dark">{date}</time>}
+      {date && (
+        <time className="text-text-light dark:text-text-dark md:whitespace-nowrap">
+          {date}
+        </time>
+      )}
     </>
   );
 
@@ -29,8 +38,8 @@ function Items({ area, company, logo, url, date, key }) {
       {url ? (
         <a
           href={url}
-		      target="_blank"
-		      rel="noreferrer"
+          target="_blank"
+          rel="noreferrer"
           className="flex justify-between w-full px-3 py-2 -mx-3 -my-2 no-underline"
         >
           {content}
@@ -42,10 +51,8 @@ function Items({ area, company, logo, url, date, key }) {
   );
 }
 
-export default function ShowItems( {items} ) {
+export default function ShowItems({ items }) {
   return (
-    <ul className="flex flex-col gap-8 animated-list">
-      {items.map(Items)}
-    </ul>
+    <ul className="flex flex-col gap-8 animated-list">{items.map(Items)}</ul>
   );
 }
